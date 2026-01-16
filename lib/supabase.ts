482835aa86aa0e26e0d8b43d1629e5ba9f0a0775
coljmtn <87839838+coljmtn@users.noreply.github.com>
2026-01-16 15:58:15 +0200

    Add files via upload

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://ryagsmqnmdpmtphkenjy.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5YWdzbXFubWRwbXRwaGtlbmp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg1NjI2MDAsImV4cCI6MjA4NDEzODYwMH0.LcVhWerN5mx8lTMII3lqJS1GzzitGQj5Y5sm3WwuqAYT';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5YWdzbXFubWRwbXRwaGtlbmp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg1NjI2MDAsImV4cCI6MjA4NDEzODYwMH0.LcVhWerN5mx8lTMII3lqJS1GzzitGQj5Y5sm3WwuqAY';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY.trim());
