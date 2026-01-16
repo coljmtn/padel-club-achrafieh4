efbf933d6139d24e0dc3d813d2de49f176d29081
coljmtn <87839838+coljmtn@users.noreply.github.com>
2026-01-16 15:15:04 +0200

    Add files via upload

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -11,9 +11,9 @@ import { createClient } from '@supabase/supabase-js';
  */
 
 // Votre URL est correcte
-const SUPABASE_URL = 'https://ryagsmqnmdpmtphkenjy.supabase.co';
+const SUPABASE_URL = https://ryagsmqnmdpmtphkenjy.supabase.co;
 
 // REMPLACEZ CETTE VALEUR par la clé qui commence par "eyJ..."
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5YWdzbXFubWRwbXRwaGtlbmp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg1NjI2MDAsImV4cCI6MjA4NDEzODYwMH0.LcVhWerN5mx8lTMII3lqJS1GzzitGQj5Y5sm3WwuqAYT_PAR_eyJ';
+const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5YWdzbXFubWRwbXRwaGtlbmp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg1NjI2MDAsImV4cCI6MjA4NDEzODYwMH0.LcVhWerN5mx8lTMII3lqJS1GzzitGQj5Y5sm3WwuqAYT_PAR_eyJ;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
